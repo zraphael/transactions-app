e6240f24d11255fcf046086add63580740b2d9c1
zraphael <raphael@wcarvalho.com>
2022-01-22 13:47:25 -0300

    Inicio do desenvolvimento da aplicacao, com a base + inicio da criacao do component TransactionsList

---
+++ src/components/TransactionsList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TransactionsList = () => {
+
+    return(
+        <Title>Listagem de Transações</Title>
+    );
+};
+
+const Title = styled.div`
+    font: roboto, sans-serif;
+`
+
+export default TransactionsList;
